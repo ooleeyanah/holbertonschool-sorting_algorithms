2b14c3633cfe7c2fa99a604c2ec2bb9db255d6d8
Uliana Deshin <uliana.deshin@gmail.com>
2025-12-13 05:02:20 +0000

    selection sort i hope

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts int array selection asc
+ * @array: array
+ * @size: size of array
+ * Return: no return
+ */
+void selection_sort(int *array, size_t size)
+{
+size_t i, j, min_idx;
+int temp;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+min_idx = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_idx])
+min_idx = j;
+}
+if (min_idx != i)
+{
+temp = array[i];
+array[i] = array[min_idx];
+array[min_idx] = temp;
+print_array(array, size);
+}
+}
+}
